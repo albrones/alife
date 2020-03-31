f3deced8cde233f053669e4a0ad81d83b897f0bc
Alexis Brones <alexis.brones@zenika.com>
2020-03-31 15:55:37 +0200

    Clean Home, About & template

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
     <div id="app">
+        <div class="logo">
+            <h1>ALIFE</h1>
+        </div>
         <div id="nav">
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link>

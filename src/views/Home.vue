9158194098efc6215dd7abbef3cddc6505599fc4
Alexis Brones <alexis.brones@zenika.com>
2020-04-29 17:29:30 +0200

    Add instructions in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,27 @@
 <template>
     <div class="home">
-        <h2>This is my homepage</h2>
+        <h2>Bienvenue !</h2>
+        <h3>Ce blog est en cours de construction.</h3>
+        <h3>
+            Merci de suivre les instructions suivantes:
+        </h3>
+        <ol>
+            <li>
+                <b
+                    >Vider le cache navigateur manuellement pour avoir la
+                    dernière version:</b
+                >
+                Si vous êtes sur Mac il y a le raccourci "Cmd+Shift+R" (sur
+                Windows Ctrl+Shift+R)
+            </li>
+        </ol>
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
     name: 'Home',
 }
 </script>
+
+<style lang="scss" scoped></style>

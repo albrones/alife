96d4a00215a6a530a3f61f39b64476aca2ee2038
Alexis Brones <alexis.brones@zenika.com>
2020-04-30 23:02:34 +0200

    Add edit button on recette view

--- src/components/recette/Recette.vue
+++ src/components/recette/Recette.vue
@@ -16,6 +16,12 @@
         <!-- TODO:
             Autres Photos (galerie)
          -->
+        <ButtonRouter
+            class="button-edit"
+            :path="'/recettes/edit/' + recette.id"
+        >
+            <Edit />
+        </ButtonRouter>
     </div>
 </template>
 
@@ -23,7 +29,9 @@
 import database from '@/firebase/db'
 
 import Astuces from '@/components/recette/Astuces'
+import ButtonRouter from '@/components/ui/ButtonRouter'
 import Categories from '@/components/recette/Categories'
+import Edit from '@/components/ui/png/Edit'
 import ImageRecette from '@/components/recette/ImageRecette'
 import InfosPratiques from '@/components/recette/InfosPratiques'
 import InfosPrincipales from '@/components/recette/InfosPrincipales'
@@ -37,7 +45,9 @@ export default {
     name: 'Recette',
     components: {
         Astuces,
+        ButtonRouter,
         Categories,
+        Edit,
         ImageRecette,
         InfosPratiques,
         InfosPrincipales,
@@ -51,25 +61,24 @@ export default {
         return {
             isLoaded: false,
             recette: {
-                recette: {
-                    astuces: [],
-                    categories: [],
-                    date: '',
-                    images: [],
-                    infosPratiques: { preparation: '', repos: '', cuisson: '' },
-                    infosPrincipales: { auteur: '', subtitle: '', title: '' },
-                    ingredients: {
-                        principaux: [],
-                        secondaires: {
-                            data: [],
-                            title: '',
-                        },
+                astuces: [],
+                categories: [],
+                date: '',
+                id: this.$route.params.id,
+                images: [],
+                infosPratiques: { preparation: '', repos: '', cuisson: '' },
+                infosPrincipales: { auteur: '', subtitle: '', title: '' },
+                ingredients: {
+                    principaux: [],
+                    secondaires: {
+                        data: [],
+                        title: '',
                     },
-                    instructions: [],
-                    materielConseille: [],
-                    variantes: [],
-                    video: '',
                 },
+                instructions: [],
+                materielConseille: [],
+                variantes: [],
+                video: '',
             },
         }
     },
@@ -103,5 +112,9 @@ export default {
 }
 .content-recette {
     align-items: unset;
+}
+.button-edit {
+    align-self: center;
+    margin: 16px 0;
 }
 </style>
